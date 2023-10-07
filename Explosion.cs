7570927add07e45b588460f55562fb45a3e44cff
Fangzhu Deng <dengfz@gmail.com>
2023-10-06 22:42:10 -0600

    Adjust timers and add tween for explosion.

--- Explosion.cs
+++ Explosion.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class Explosion : Node2D
 {
+	private void _Ready()
+	{
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(GetNode<Sprite2D>("Sprite2D"), "scale", Vector2.Zero, 1.5f).SetTrans(Tween.TransitionType.Linear);
+	}
 	private void OnExpTimer()
 	{
 		QueueFree();
