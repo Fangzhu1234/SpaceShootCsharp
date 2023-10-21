b1d42032f7ebd14a98954522e6f4e3c7955d652f
Fangzhu Deng <dengfz@gmail.com>
2023-10-20 21:21:37 -0600

    Fix a minor error of _ready()

--- Explosion.cs
+++ Explosion.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Explosion : Node2D
 {
-	private void _Ready()
+	public override void _Ready()
 	{
 		Tween tween = GetTree().CreateTween();
 		tween.TweenProperty(GetNode<Sprite2D>("Sprite2D"), "scale", Vector2.Zero, 1.5f).SetTrans(Tween.TransitionType.Linear);
